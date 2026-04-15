5563a70541018606b4712d4c08f5a1b375ed5d7b
ansarifaiyaz28 <ansati.faiyaz28@gmail.com>
2026-04-15 17:24:14 -0400

    checking how arguments works

--- call_main_programatically.py
+++ call_main_programatically.py
@@ -6,15 +6,40 @@
 #     print_comms=True
 # )
 
-import subprocess
+# import subprocess
 
-cmd = [
-    "poetry", "run", "python3", "run_from_config.py",
-    "--config_path", "./task_configs/nmap.toml",
-    "--elicitation_index", "1",
-    # "--agent_identifier", "openai",
-    # "--model_name", "gpt-4o-2024-08-06",
-    "--print_comms", "true"
+# cmd = [
+#     "poetry", "run", "python3", "run_from_config.py",
+#     "--config_path", "./task_configs/nmap.toml",
+#     "--elicitation_index", "1",
+#     # "--agent_identifier", "openai",
+#     # "--model_name", "gpt-4o-2024-08-06",
+#     "--print_comms", "true"
+# ]
+
+# subprocess.run(cmd)
+
+import subprocess
+from itertools import product
+
+config_paths = [
+    "./task_configs/nmap.toml",
+    "./task_configs/binpwn_gdb_repl.toml"
 ]
 
-subprocess.run(cmd)
+elicitation_indices = ["0", "1", "2"]
+agent = ["anthropic"]
+models = ["claude-sonnet-4-6"]
+
+for config, idx, model in product(config_paths, elicitation_indices, models):
+    cmd = [
+        "poetry", "run", "python3", "run_from_config.py",
+        "--config_path", config,
+        "--elicitation_index", idx,
+        "--agent_identifier", agent,
+        "--model_name", model,
+        "--print_comms", "true"
+    ]
+
+    print(f"Running: {config}, idx={idx}, model={model}")
+    subprocess.run(cmd)
