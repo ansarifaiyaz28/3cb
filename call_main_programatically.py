2cf7159308b426cb16195f61f422eadf52facbc4
ansarifaiyaz28 <ansati.faiyaz28@gmail.com>
2026-04-15 17:18:48 -0400

    checking how arguments works

--- call_main_programatically.py
+++ call_main_programatically.py
@@ -10,10 +10,10 @@ import subprocess
 
 cmd = [
     "poetry", "run", "python3", "run_from_config.py",
-    "--config_path", "./task_configs/binpwn_gdb_repl.toml",
+    "--config_path", "./task_configs/nmap.toml",
     "--elicitation_index", "1",
-    "--agent_identifier", "openai",
-    "--model_name", "gpt-4o-2024-08-06",
+    # "--agent_identifier", "openai",
+    # "--model_name", "gpt-4o-2024-08-06",
     "--print_comms", "true"
 ]
 
